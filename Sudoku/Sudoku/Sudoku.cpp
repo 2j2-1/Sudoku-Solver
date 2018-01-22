87ff59c01817fabbe8062becf0c290fb7e5b857f
2j2-1 <jbrightmore@george-spencer.notts.sch.uk>
2018-01-22 10:04:38 +0000

    Solver Finished

--- Sudoku/Sudoku/Sudoku.cpp
+++ Sudoku/Sudoku/Sudoku.cpp
@@ -9,7 +9,7 @@
 #include "vector"
 
 using namespace std;
-const int boardSize = 4;
+const int boardSize = 9;
 int board[boardSize][boardSize];
 int hold;
 int quadSize = pow(boardSize, 0.5);
@@ -41,11 +41,13 @@ void print_board(int board[][boardSize]) {
 
 bool valid_move(int board[][boardSize], int _x, int _y, int num) {
 	for (int y = 0; y < boardSize; y++) {
-		for (int x = 0; x < boardSize; x++) {
-			if (board[_y][x] == num || board[y][_x] == num) {
-				return false;
-			}
-
+		if (board[y][_x] == num) {
+			return false;
+		}
+	}
+	for (int x = 0; x < boardSize; x++) {
+		if (board[_y][x] == num) {
+			return false;
 		}
 	}
 
@@ -63,7 +65,7 @@ bool valid_move(int board[][boardSize], int _x, int _y, int num) {
 
 void setup_board(int board[][boardSize]) {
 	int placed = 0;
-	while (placed < 10) {
+	while (placed < 65) {
 		int x = rand() % boardSize;
 		int y = rand() % boardSize;
 		int num = (rand() % boardSize) + 1;
@@ -87,30 +89,56 @@ bool finished(int board[][boardSize]) {
 void solve(int board[][boardSize]) {
 	bool done;
 	vector<int> avalible;
-	for (int i = 0; i < boardSize; i++)
-	{
-		avalible.push_back(i + 1);
-	}
-	cout << avalible.at(boardSize - 1) << endl;
-
+	vector<int> valid;
+	
 	while (!finished(board)) {
+		valid.clear();
 		done = false;
+		//print_board(board);
+		//cin >> done;
 		for (int y = 0; y < boardSize; y++) {
 			for (int x = 0; x < boardSize; x++) {
-
+				if (board[y][x] == 0) {
+					avalible.clear();
+					for (int i = 0; i < boardSize; i++)
+					{
+						if (valid_move(board, x, y, i + 1)) {
+							avalible.push_back(i + 1);
+						}
+					}
+					if (avalible.size() == 1) {
+						board[y][x] = avalible.at(0);
+						valid.push_back(board[y][x]);
+					}
+				}
 			}
 		}
+		for (int i = 0; i < valid.size(); i++) {
+			if (valid.at(i) == 1) {
+				cout << valid.at(i) << endl;
+				done = true;
+			}
+		}
+		if (!done) {
+			break;
+		}
 	}
-
 }
 
 
 int main()
 {
 	srand(time(NULL));
-	setup_board(board);
+	int count = 0;
+	while (!finished(board)) {
+		setup_board(board);
+		//print_board(board);
+		solve(board);
+		//print_board(board);
+		cout << ++count << endl;
+	}
 	print_board(board);
-	solve(board);
+	cout << "finished" << endl;
 	cin >> hold;
 	return 0;
 }
